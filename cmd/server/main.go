7d2a33a01f304cdd68abae8555fc8a6f852a64a9
mikequentel <mikequentel@yahoo.com>
2026-02-11 18:24:19 -0500

    fixup! fixup! fixup! fixup! fixup! mikequentel/taskusama#1: initial add of project skeleton for taskusama

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,46 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
 func main() {
+	// Config
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
+
+	// DB Pool
+	ctx := context.Background()
+
+	dbpool, err := pgxpool.New(ctx, dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dbpool.Close()
+
+	// Test DB
+	if err := dbpool.Ping(ctx); err != nil {
+		log.Fatal("DB not reachable:", err)
+	}
+
+	// Gin
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	log.Println("Listening on :8080")
+	r.Run(":8080")
 }
+
